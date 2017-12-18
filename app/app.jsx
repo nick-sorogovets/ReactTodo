6a59376b19016a9c0c5df1b8186251391e153913
Nickolay Sorogovets <sorogovets.nikolay@gmail.com>
2017-12-18 10:52:17 +0300

    Add Subscribe devTools

--- app/app.jsx
+++ app/app.jsx
@@ -14,3 +14,5 @@ ReactDOM.render(
   <TodoApp/>,
   document.getElementById('app')
 );
+
+import './redux-todo-example';
---
+++ app/redux-todo-example.jsx
@@ -0,0 +1,48 @@
+const redux = require('redux');
+
+console.log('Starting todo redux example');
+
+const stateDefault = {
+  searchText: '',
+  showCompleted: false,
+  todos: []
+};
+const reducer = (state = stateDefault, action) => {
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
+};
+const store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+const unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  document.getElementById('app').innerHTML = state.searchText;
+});
+// unsubscribe();
+
+console.log('currentState', store.getState());
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'work'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'dog'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Something else'
+}); 
